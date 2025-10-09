7e11823081535b033b1d181b311630dca50d1a45
Ysa-04 <ysaura2903@outlook.com>
2025-10-09 15:47:24 +0200

    nieuwe oef

---
+++ H14-PostOffice/RegisteredLetter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H14_PostOffice
+{
+    internal class RegisteredLetter
+    {
+		private double distance;
+		public double Distance
+		{
+			get { return distance; }
+			set 
+			{  
+				if (value > 0)
+				{
+                    distance = value;
+                }
+			}
+		}
+
+	}
+}
