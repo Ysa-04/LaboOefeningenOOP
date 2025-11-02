52bd2fce917dab902a0b68984cf01232efbfc724
Ysa-04 <ysaura2903@outlook.com>
2025-11-02 11:07:13 +0100

    oefening klaar

---
+++ H10-LeapYearCount/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace H10_LeapYearCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CountLeapYears();
+        }
+        public static void CountLeapYears()
+        {
+            int count = 0;
+           for (int year = 1799; year <= 2021; year++)
+           {
+                DateTime.IsLeapYear(year);
+                count++;
+           }
+           Console.WriteLine($"Aantal schrikkeljaren tussen 1799 en 2021: {count}");
+        }
+    }
+}
+           
+        
+           
+  
