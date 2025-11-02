4b1011574b088f5251aca987624210902bbb6219
Ysa-04 <ysaura2903@outlook.com>
2025-11-02 10:45:16 +0100

    git ge hebt commitment issues maat

---
+++ H10-TicksSince/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace H10_TicksSince
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan diff = DateTime.Today - new DateTime(2000, 1, 1);
+            Console.WriteLine($"Sinds 1 januari 2000 zijn er {diff.Ticks} ticks voorbij gegaan.");
+
+        }
+    }
+}
---
+++ H10-TicksSince2000/Program.cs
@@ -0,0 +1,14 @@
+namespace H10_TicksSince2000
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = Host.CreateApplicationBuilder(args);
+            builder.Services.AddHostedService<Worker>();
+
+            var host = builder.Build();
+            host.Run();
+        }
+    }
+}
